a23c32a0ae9d8945b0f9802f5299a9d4446360c0
Ana Andric <aandric@z3r5p5.42lyon.fr>
2022-09-30 13:23:08 +0200

    feat(check input) : added function to check if the extension file is .cub

--- src/check_input.c
+++ src/check_input.c
@@ -7,6 +7,11 @@ int	check_input(int ac, char **av)
 		printf("Please select a scene in arguments");
 		return (0);
 	}
+	if (!ft_strnstr(av[1], ".cub", ft_strlen(av[1])))
+	{
+		printf("Not a .cub scene");
+		return (0);
+	}
 	if (open(av[1], O_RDONLY) < 0)
 	{
 		printf("Unreadable scene file");
